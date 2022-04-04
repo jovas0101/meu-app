782017c66937a863c5a5ab38deaf5b950ed92979
Gabriel Aquino <40728047+aq-gabriel@users.noreply.github.com>
2022-04-04 15:02:08 -0300

    feat: initial files

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+const data = require('./db/data-old.json')
+
+app.get('/', (require, response) => {
+    response.send('Hello world Gio tops')
+})
+
+app.get('/abc', (require, response) => {
+    //  data : nome, idade, cidade
+    const { nome, idade, cidade } = data
+
+    const info = `Nome: ${nome}, Idade: ${idade}, Cidade: ${cidade}`
+    response.send(info)
+})
+
+app.get('/vendas', (require, response) => {
+   // write your code ...
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
